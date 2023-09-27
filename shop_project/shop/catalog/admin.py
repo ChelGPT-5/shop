f39cbb9d25ac01fe92bf0204082eff56d80fdb97
Valiantsin <125600788+ChelGPT-5@users.noreply.github.com>
2023-09-27 21:45:08 +0300

    Revert "Feature/add guest endpoints"

--- shop_project/shop/catalog/admin.py
+++ shop_project/shop/catalog/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from catalog.models import Category, Discount, Product, Seller, Promocode
 
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'articul', 'category', 'seller')
-    search_fields = ('name', 'articul', 'seller__name', 'category__name', 'seller__country')
-    list_select_related = ('category', 'seller')
-
-
-admin.site.register(Category)
-admin.site.register(Discount)
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Promocode)
-admin.site.register(Seller)
 # Register your models here.
